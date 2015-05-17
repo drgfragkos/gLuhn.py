9cec27f47f61fb7b38665e2d30a84201c3714d3b
drgfragkos <gfragkos@gmail.com>
2015-05-17 15:27:14 +0100

    Check/Generate PAN based on Luhn algorithm
    Given any Primary Account Number (PAN), the application will check if it
    is valid by using
    the Luhn algorithm. If the PAN contains any number of question mark (?)
    characters
    e.g.: 12345555?23412?4, all valid combinations for that PAN will be
    generated.

--- README.md
+++ README.md
@@ -1,19 +1,39 @@
+
+############################################################################################
+## You may modify, reuse and distribute the code freely as long as it is referenced back  ##
+## to the author using the following line: ..based on gLuhn.py by @drgfragkos             ##
+
 gLuhn.py v0.7 - Check/Generate PAN based on Luhn algorithm (c)gfragkos 2013  
 
 usage: gLuhn.py [PAN]
-Given any Primary Account Number (PAN), the application will check if it is valid by using the Luhn algorithm.
-If the PAN contains any number of question mark (?) characters e.g.: 12345555?23412?4, all valid combinations for that PAN will be generated.
-                
-I couldn't find a tool capable of generating all possible combinations of valid card numbers (PAN) while specific digits in the PAN are known already. The tool can be used for validating a single PAN or it can generate all valid combinations for a partially known PAN. 
+Given any Primary Account Number (PAN), the application will check if it is valid by using
+the Luhn algorithm. If the PAN contains any number of question mark (?) characters 
+e.g.: 12345555?23412?4, all valid combinations for that PAN will be generated.
 
-version 0.7.0
-Initial release version. 
+I couldn't find a tool capable of generating all possible combinations of valid card 
+numbers (PAN) while specific digits in the PAN are known already. The tool can be used 
+for validating a single PAN or it can generate all valid combinations for a partially 
+known PAN(s). 
+
+
+Version:
+0.7.0 : 2015/05/15 - Released on GitHub. 
+0.6.0 : 2013/02/28 - Initial version not publicly released.
+
+
+Dependencies:
+You need to have numpy installed.
+In case you need to check your Python version: python --version
+Check if you have numpy: apt-cache policy python-numpy
+Install numpy: apt-get install python-numpy
+
 
 To Do:
 Include checks for IIN and BIN
 
-
-
+##                                                                                        ##
+##                                                                                        ##
+############################################################################################
 
 
 
--- gLuhn.py
+++ gLuhn.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+
+## gLuhn.py version 0.7 - Check/Generate PAN based on the Luhn algorithm (c)gfragkos 2013  ##
+## You may modify, reuse and distribute the code freely as long as it is referenced back   ##
+## to the author using the following line: ..based on gLuhn.py by @drgfragkos              ##
+
 import sys
 import numpy
 import re
@@ -20,29 +25,24 @@ def LuhnChk(card_number):
 
 def GenDigits(card_number):
     if card_number.count("?") > 0 and len(card_number) <= 16:
-	possibilities = numpy.uint64(10**card_number.count("?"))
+	possibilities = numpy.uint64(10**card_number.count("?"))  ## Precalculate the number of combinations
 	print "Attempting to generate " + str(possibilities) + " PAN combinations for: " + str(card_number)
-	##counter = numpy.uint64(0) ## declaring a HUGE counter: Unsigned integer (0 to 18446744073709551615)
 	counter = 0
 
-	PANasList = list(card_number) # Make the PAN number a List
+	PANasList = list(card_number)     ## Convert the PAN number into a List
 
-	L = []
+	L = []                            ## A list with the indexes of the ? characters as found in given PAN
 	for m in re.finditer('\?', card_number):
 		L.append(m.start())
-	##print L ## A list with the indexes of the ? characters as found in given PAN
 		
-	TotalPANs = 0 ##To count how many of the generated PAN combinations are actually valid with Luhn
-	STR_LIST = [] ##A list for the generated combinations of digits
-	while (counter < possibilities):
-		##print counter,"\n"
+	TotalPANs = 0                     ## Count the number of valid generated PAN combinations
+	STR_LIST = []                     ## A list for the generated combinations of digits
+	while (counter < possibilities):  
 		if len(str(counter)) < card_number.count("?") + 1:
 			m = card_number.count("?") - len(str(counter))
-			##print  "0" * m + str(counter)
 			STR = "0" * m + str(counter)
 			
 		else:
-			##print str(counter)
 			STR = str(counter)
 		
 		STR_LIST = STR
@@ -53,7 +53,6 @@ def GenDigits(card_number):
 			c = c + 1	
 			PANasList[item]=STR_LIST[c-1]
 
-		##print "".join(PANasList) ##Debug. Output generated PAN
 		tempPAN = "".join(PANasList)
 		if LuhnChk(tempPAN):
 			print "[+] Valid PAN ",tempPAN
@@ -68,25 +67,24 @@ def GenDigits(card_number):
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-	print "gLuhn.py v0.7 - Check/Generate PAN based on Luhn algorithm (c)gfragkos 2013  "
+	print "gLuhn.py v0.7 - Check/Generate PAN based on Luhn algorithm (c)gfragkos 2013 "
 	print ""
         print "usage:", sys.argv[0],"[PAN]"
-	print "      PAN       Given any Primary Account Number (PAN), the application will "
-        print "                check if is valid by using the Luhn algorithm.               "
-        print "                If the PAN contains any number of questionmark (?) characters"
-	print "                e.g.: 12345555?23412?4, all valid combinations for that PAN  "
-        print "                will be generated.                                           " 
+	print "      PAN     Given any Primary Account Number (PAN), the application will  "
+        print "              check if it is valid by using the Luhn algorithm.             "
+        print "              If the PAN contains any number of question mark (?) characters"
+	print "              e.g.: 12345555?23412?4, all valid combinations for that PAN   "
+        print "              will be generated.                                            " 
         exit(0);
 
-    if len(sys.argv[1]) <= 16 and sys.argv[1].isdigit():  ## Validate a PAN with Luhn
+    if len(sys.argv[1]) <= 16 and sys.argv[1].isdigit():      ## Validate a PAN with Luhn
 	if LuhnChk(sys.argv[1]):
 		print "[+] Valid PAN ",LuhnChk(sys.argv[1])
 	else:
 		print "[-] Invalid PAN ",LuhnChk(sys.argv[1])
 
-    else:  ## Generate PANs 
+    else:                                                     ## Generate PANs 
 	if len(sys.argv[1]) <= 16 and re.match("^[0-9\?]*$",sys.argv[1]): 
-	##result = re.match(pattern, string)
 		print GenDigits(sys.argv[1].strip())
         		
 
